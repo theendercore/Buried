b7e07548385fcfdb298c11831fb35930a7f1d187
TheEnderCore <theendercore@gmail.com>
2025-11-07 13:40:45 +0200

    BAttachmentTypes suppressed and removed default

--- src/main/kotlin/com/theendercore/buried/init/BAttachmentTypes.kt
+++ src/main/kotlin/com/theendercore/buried/init/BAttachmentTypes.kt
@@ -5,11 +5,12 @@ import com.theendercore.buried.core.component.GraveData
 import net.fabricmc.fabric.api.attachment.v1.AttachmentRegistry
 import net.fabricmc.fabric.api.attachment.v1.AttachmentType
 
+@Suppress("UnstableApiUsage")
 object BAttachmentTypes {
     fun init() = Unit
 
     @JvmField
     val GRAVE_DATA: AttachmentType<List<GraveData>> = AttachmentRegistry.create(id("grave_data")) {
-        it.initializer { listOf() }.persistent(GraveData.CODEC.listOf())
+        it.persistent(GraveData.CODEC.listOf())
     }
 }
